c3b81a6c4a0140676371345eb516174b0633088a
HashimAziz <hashimkayani97@gmail.com>
2021-03-06 11:33:29 +0500

    conditional rendering (multiple return, short circuit evalution and ternary operator, show and hide component)

--- src/tutorial/3-conditional-rendering/setup/3-show-hide.js
+++ src/tutorial/3-conditional-rendering/setup/3-show-hide.js
@@ -1,7 +1,40 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState, useEffect } from "react";
 
 const ShowHide = () => {
-  return <h2>show/hide</h2>;
+  const [show, setShow] = useState(false);
+  return (
+    <>
+      <button className="btn" onClick={() => setShow(!show)}>
+        show/hide
+      </button>
+      {/* toggle the component using short circuit evaluation  && operator */}
+      {show && <Item />}
+    </>
+  );
+};
+
+const Item = () => {
+  const [size, setSize] = useState(window.innerWidth);
+
+  const checksize = () => {
+    setSize(window.innerWidth);
+  };
+
+  useEffect(() => {
+    window.addEventListener("resize", checksize);
+
+    //clean up function
+    return () => {
+      window.removeEventListener("resize", checksize);
+    };
+  }, []);
+
+  return (
+    <div style={{ marginTop: "2rem" }}>
+      <h1>window</h1>
+      <h2>size: {size} px</h2>
+    </div>
+  );
 };
 
 export default ShowHide;
