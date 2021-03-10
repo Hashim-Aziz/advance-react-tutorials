2401b7572e2c2b993127d315db9bccf284e499b9
HashimAziz <hashimkayani97@gmail.com>
2021-03-10 09:30:44 +0500

    add item to list

--- src/tutorial/4-forms/setup/1-controlled-inputs.js
+++ src/tutorial/4-forms/setup/1-controlled-inputs.js
@@ -8,9 +8,20 @@ import React, { useState } from "react";
 const ControlledInputs = () => {
   const [firstname, setFirstName] = useState("");
   const [email, setEmail] = useState("");
+  const [people, setPeople] = useState([]);
+
   const handleSubmit = (e) => {
     e.preventDefault();
-    console.log(firstname, email);
+    if (firstname && email) {
+      const person = { id: new Date().getTime().toString(), firstname, email };
+      setPeople((people) => {
+        return [...people, person]; // [...people,person] (...) spread operator preserve the array value then add new in existing value
+      });
+      setFirstName("");
+      setEmail("");
+    } else {
+      console.log("submit empty value");
+    }
   };
   return (
     <>
@@ -38,6 +49,15 @@ const ControlledInputs = () => {
           </div>
           <button type="submit">add person</button>
         </form>
+        {people.map((person, index) => {
+          const { id, firstname, email } = person;
+          return (
+            <div className="item" key={id}>
+              <h4>{firstname}</h4>
+              <p>{email}</p>
+            </div>
+          );
+        })}
       </article>
     </>
   );
