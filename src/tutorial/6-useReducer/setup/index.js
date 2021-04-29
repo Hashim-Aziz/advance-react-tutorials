a5803fbd2ff3b7b9b13c29549e77df28573992f9
HashimAziz <hashimkayani97@gmail.com>
2021-04-29 09:26:50 +0500

    learning useReducer

--- src/tutorial/6-useReducer/setup/index.js
+++ src/tutorial/6-useReducer/setup/index.js
@@ -1,10 +1,49 @@
-import React, { useState, useReducer } from 'react';
-import Modal from './Modal';
-import { data } from '../../../data';
+import React, { useState, useReducer } from "react";
+import Modal from "./Modal";
+import { data } from "../../../data";
 // reducer function
 
+const reducer = (state, action) => {};
+
+const defaultstate = {
+  people: [],
+  isModalOpen: false,
+  modalContent: "",
+};
+
 const Index = () => {
-  return <h2>useReducer</h2>;
+  const [name, setName] = useState("");
+  const [state, dispatch] = useReducer(reducer, defaultstate);
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    if (name) {
+    } else {
+    }
+  };
+
+  return (
+    <>
+      {state.isModalOpen && <Modal modalContent={state.modalContent} />}
+      <form onSubmit={handleSubmit} className="form">
+        <div>
+          <input
+            type="text"
+            value={name}
+            onChange={(e) => setName(e.target.value)}
+          />
+        </div>
+        <button type="submit">add</button>
+      </form>
+      {state.people.map((peep) => {
+        return (
+          <div key={peep.id}>
+            <h4>{peep.name}</h4>
+          </div>
+        );
+      })}
+    </>
+  );
 };
 
 export default Index;
